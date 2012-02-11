98794fd2eb2ed0843fcd4c0ba874fc30b97cd0e3
cj <cjlazell@googlemail.com>
2012-02-10 18:00:25 -0600

    Improved finding view files for render.
    It now works with controllers that use map and pretty much every other case.

--- lib/padrino-responders/helpers/controller_helpers.rb
+++ lib/padrino-responders/helpers/controller_helpers.rb
@@ -7,50 +7,50 @@ module Padrino
         #
         def notify(kind, message, *args, &block)
           settings.notifier.say(self, kind, message, *args, &block) if settings.notifier
-        end 
-        
+        end
+
         ##
         # Trys to render and then falls back to to_format
         #
-        def try_render(object)  
+        def try_render(object)
           begin
-            render "#{controller_name}/#{action_name}"
+            render self.route.instance_variable_get('@original_path').gsub(/(\(|\/:).+/, '/') + action_name
           rescue
             case content_type
-            when :json  
+            when :json
               return object.to_json if object.respond_to?(:to_json)
-            when :xml      
-              return object.to_xml if object.respond_to?(:to_xml)       
-            end      
+            when :xml
+              return object.to_xml if object.respond_to?(:to_xml)
+            end
             raise ::Padrino::Responders::ResponderError, "Couldn't figure out a way to respond to this."
           end
         end
-        
+
         ##
         # Returns name of current action
         #
         def action_name
-          name = self.request.route_obj.instance_variable_get('@named').to_s    
+          name = self.request.route_obj.instance_variable_get('@named').to_s
           name.gsub!(/^#{controller_name}_?/, '')
           name = 'index' if name == ''
           name
         end
-        
+
         ##
         # Returns name of current controller
         #
-        def controller_name   
+        def controller_name
           self.request.route_obj.instance_variable_get('@controller')
         end
-        
+
         ##
-        # Returns translated, human readable name for specified model. 
+        # Returns translated, human readable name for specified model.
         #
         def human_model_name(object)
           if object.class.respond_to?(:human)
             object.class.human
           elsif object.class.respond_to?(:human_name)
-            object.class.human_name 
+            object.class.human_name
           else
             t("models.#{object.class.to_s.underscore}", :default => object.class.to_s.humanize)
           end
